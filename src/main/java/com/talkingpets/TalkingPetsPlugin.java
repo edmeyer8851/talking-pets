f8bbc73ec0af12e5037490a2a65a20a9000b86cb
Eddie Meyer <edmeyer8851@gmail.com>
2023-02-08 11:23:30 -0500

    initial commit

--- src/main/java/com/talkingpets/TalkingPetsPlugin.java
+++ src/main/java/com/talkingpets/TalkingPetsPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.talkingpets;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Talking Pets"
 )
-public class ExamplePlugin extends Plugin
+public class TalkingPetsPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private TalkingPetsConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	TalkingPetsConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(TalkingPetsConfig.class);
 	}
 }
